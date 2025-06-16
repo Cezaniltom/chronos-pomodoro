9cfddbf50983d87950c89862c230fac2724eb2c1
Cezaniltom <cezaniltom@outlook.com>
2025-06-16 10:57:47 -0300

    utilizando css em escopo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './styles/theme.css'
 import './styles/global.css'
+import { Heading } from './components/Heading'
 
 export function App() {
     console.log('Hello World')
 
     return (
         <>
+            <Heading>Olá Mundo 1</Heading>
+            <Heading>Olá Mundo 2</Heading>
+            <Heading>Olá Mundo 3</Heading>
+            <Heading>Olá Mundo 4</Heading>
             <p>Hello World</p>
             <p>Hello World 2</p>
         </>
