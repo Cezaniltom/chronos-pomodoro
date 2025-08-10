cc277dfbdad8fd01db6afbcb1639f30e1db6faae
Cezaniltom <cezaniltom@outlook.com>
2025-08-10 15:44:42 -0300

    Add components Cycles e DefaultButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react'
 import { Container } from './components/Container'
 import { CountDown } from './components/CountDown'
+import { Cycles } from './components/Cycles'
+import { DefaultButton } from './components/DefaultButton'
 import { DefaultInput } from './components/DefaultInput'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
@@ -41,12 +44,12 @@ export function App() {
           </div>  
 
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0 0 0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className="formRow">
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color='green'/>
+            <DefaultButton icon={<StopCircleIcon />} color='red'/>
           </div>
           
         </form>
