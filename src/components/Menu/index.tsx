1283562aa4fb05ab0647b97c0cdd643b68d258b4
Cezaniltom <cezaniltom@outlook.com>
2026-09-01 14:23:18 -0300

    Adicionando o prevent default no link do tema

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,8 +1,18 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
 
+type ThemeColor = 'dark' | 'light'
 
 export function Menu() {
+
+    const [theme, setTheme] = useState<ThemeColor>('dark')
+
+    function handleClickTheme(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        e.preventDefault()
+        console.log('vendo o estado')
+    }
+
     return <nav className={styles.menu}>
         <a className={styles.menuLink} href="#">
             <HouseIcon />
@@ -13,7 +23,7 @@ export function Menu() {
         <a className={styles.menuLink} href="#">
             <SettingsIcon />
         </a>
-        <a className={styles.menuLink} href="#">
+        <a className={styles.menuLink} href="#" onClick={handleClickTheme}>
             <SunIcon />
         </a>
     </nav>;
