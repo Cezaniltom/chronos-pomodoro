c4fa31f2978e14a47bacdc9450521e6b881c581c
Cezaniltom <cezaniltom@outlook.com>
2026-09-01 12:14:26 -0300

    Aceitando merge de entrada

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-=======
 import './styles/global.css'
 import './styles/theme.css'
->>>>>>> developer
 import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
