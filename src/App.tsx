c7f650f4879c679b2bae4812bdbe27f13f43ec7f
Cezaniltom <cezaniltom@outlook.com>
2026-09-06 21:38:25 -0300

    Criando o componente do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react'
 import { Container } from './components/Container'
 import { CountDown } from './components/CountDown'
-import { Cycles } from './components/Cycles'
-import { DefaultButton } from './components/DefaultButton'
-import { DefaultInput } from './components/DefaultInput'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
+import { MainForm } from './components/MainForm'
 
 import './styles/global.css'
 import './styles/theme.css'
@@ -28,31 +25,7 @@ export function App() {
       </Container>
 
       <Container>
-        <form className='form' action="">
-          <div className="formRow">
-            <DefaultInput 
-            labelText='task' 
-            id='meuInput' 
-            type='text'
-            placeholder='Digite algo'
-            defaultValue='Valor preenchido'
-          />
-          </div>
-
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>  
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} color='green'/>
-            <DefaultButton icon={<StopCircleIcon />} color='red'/>
-          </div>
-          
-        </form>
+        <MainForm />
       </Container>
     </>
   )
