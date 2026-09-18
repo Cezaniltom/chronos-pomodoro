915ae8930a080e27a8c636b1c892166f00f4446a
Cezaniltom <cezaniltom@outlook.com>
2026-09-18 15:53:09 -0300

    Criando arquivo initialTaskState para separação de atribuições

--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, type ReactNode } from "react";
 import type { TaskStateModel } from "../../models/TaskStateModel";
+import { initialTaskState } from "./initialTaskState";
 
 type TasksContextProps = {
   state: TaskStateModel
@@ -10,28 +11,17 @@ type TasksContextProviderProps = {
   children: ReactNode
 }
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-}
+
 
 const initialContextValue = {
-  state: initialState,
+  state: initialTaskState,
   setState: () => {},
 }
 
 export const TaskContext = createContext<TasksContextProps>(initialContextValue)
 
 export function TaskContextProvider({ children }: TasksContextProviderProps) {
-  const [state, setState] = useState(initialState)
+  const [state, setState] = useState(initialTaskState)
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
