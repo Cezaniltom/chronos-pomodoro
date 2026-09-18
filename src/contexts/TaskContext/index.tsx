0ddfade1fbf1e35288dc4f3ff7a22ab94b39cf06
Cezaniltom <cezaniltom@outlook.com>
2026-09-18 16:38:24 -0300

    Criando o arquivo do hook customizado

--- src/contexts/TaskContext/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { createContext, useContext, useState, type ReactNode } from "react";
-import type { TaskStateModel } from "../../models/TaskStateModel";
-import { initialTaskState } from "./initialTaskState";
-
-export function useTaskContext() {
-  return useContext(TaskContext)
-}
