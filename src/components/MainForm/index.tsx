c47913048dbd41b6cf3e40cced80bc18f7bb3584
Cezaniltom <cezaniltom@outlook.com>
2026-09-18 16:43:04 -0300

    Removendo o button do mainform

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,25 +3,10 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { Cycles } from "../Cycles";
 import { Footer } from "../Footer";
-import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
-
-    const { setState } = useTaskContext()
-
-    function handleClick() {
-        setState(prevState => {
-            return {
-                ...prevState,
-                formattedSecondsRemaining: '21:00',
-            }  
-        })
-    }
-
     return (
         <form className='form' action="">
-
-            <button onClick={handleClick} type="button">Alterar tempo</button>
             <div className="formRow">
                 <DefaultInput 
                 labelText='task' 
