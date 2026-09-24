ffc03ab3af9744bd9b60db5f1df9940e1aa6d185
Cezaniltom <cezaniltom@outlook.com>
2026-09-24 14:36:59 -0300

    corrigindo o elemento de interrupcao de task

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -52,24 +52,23 @@ export function MainForm() {
                 tasks: [
                     ...prevState.tasks, newTask
                 ],
-
             }
-
         })
     }
 
-    function handleInterruptTask(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-
-        e.preventDefault()
-
+    function handleInterruptTask() {
         setState(prevState => {
             return {
                 ...prevState,
                 activeTask: null,
                 secondsRemaining: 0,
                 formattedSecondsRemaining: '00:00',
+                tasks: prevState.tasks.map(task => {
+                    if (prevState.activeTask && prevState.activeTask.id === task.id) {
+                        return {...task, interruptDate: Date.now()}
+                    }
+                })
             }
-
         })
     }
 
